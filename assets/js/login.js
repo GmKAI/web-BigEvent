998cc14dabc388c402dd2f0855aaa24f5f92dbc5
Gmk <842676425@qq.com>
2020-06-23 19:43:46 +0800

    注册登录页面及功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,28 +1,64 @@
 $(function () {
-    $('.link_reg').on('click', function () {
-        // console.log('aaa')
-        $('#login-box').hide();
-        $('#reg-box').show();
-    })
-
-    $('.link_login').on('click', function () {
+    $('.text-login').on('click', function () {
         $('#reg-box').hide();
         $('#login-box').show();
     })
+    $('.text-reg').on('click', function () {
 
-    $('.layui-form').on('submit', function (e) {
+        $('#reg-box').show();
+        $('#login-box').hide();
+    })
+
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6-12位'],
+        rep: function (value) {
+            var rep = $('#reg-box [name=repassword]').val()
+            if (rep !== value) {
+                return ('两次密码不一致')
+            }
+        }
+    })
+    //------------------ 监听注册页面 ------------------
+    $('#form-reg').on('submit', function (e) {
         e.preventDefault();
-        // console.log('aa');
-        var data = $(this).serialize();
-        // var username = $('#username').val()
-        // var password = $('#password').val()
+        var data = {
+            username: $('#form-reg [name=username]').val(),
+            password: $('#form-reg [name=password]').val()
+        }
         $.ajax({
             type: 'POST',
             url: 'http://ajax.frontend.itheima.net/api/reguser',
             data: data,
             success: function (res) {
-                console.log(res)
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功');
+                $('.text-login').click()
             }
-        });
-    });
-});
+        })
+    })
+
+
+    // ------------------ 监听登录页面 -------------------
+    $('#form-login').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            data: data,
+            success: function (res) {
+                // console.log(res)
+                if (res.status !== 0) {
+                    layer.msg('登陆失败')
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+})
